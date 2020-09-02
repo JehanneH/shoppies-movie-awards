67de582cb89125c7e6711f8d863a8643a7b18802
Jehanne Hodge <samarahjhodge@gmail.com>
2020-09-02 19:18:58 -0400

    implement search feature and display movies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Search from "./components/Search"
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import Search from "./components/Search";
+
+const movieApiUrl = "http://www.omdbapi.com/?s=girl&apikey=3ad7027";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(movieApiUrl)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = (searchValue) => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=3ad7027`)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      if (jsonResponse.Response === "True") {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      } else {
+        setErrorMessage(jsonResponse.Error);
+        setLoading(false);
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Search a Movie</h1>
-      <Search />
-    </div>
-  );
-}
+     <Header text="The Shoppies" />
+     <Search search={search} />
+     <div className="movies">
+       {loading && !errorMessage ? (
+        <span>loading...</span>
+        ) : errorMessage ? (
+         <div className="errorMessage">{errorMessage}</div>
+       ) : (
+         movies.map((movie, index) => (
+           <Movie key={`${index}-${movie.Title}`} movie={movie} />
+         ))
+       )}
+     </div>
+   </div>
+ );
+};
 
 export default App;
