7051fad3f31ffa9b6584b06e91719c0a3963da23
Jehanne Hodge <samarahjhodge@gmail.com>
2020-09-06 22:55:18 -0400

    add alert if there are no nominations in the list

--- src/components/Nominations.js
+++ src/components/Nominations.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from "react";
-import Movie from "./Movie";
 import { GlobalContext } from "../context/GlobalState"
 import {
   Pane,
@@ -8,7 +7,7 @@ import {
   BanCircleIcon,
   UnorderedList,
   ListItem,
-  Paragraph,
+  Alert,
 } from "evergreen-ui";
 
 export default function Nominations(props) {
@@ -24,18 +23,22 @@ export default function Nominations(props) {
           onCloseComplete={() => setShow({ isShown: false })}
           hasFooter={false}
         >
-         {nominateList.map((movie) => (
-           <p>{movie.Title}</p>
-         ))}
 
-          {/* <UnorderedList icon={BanCircleIcon} iconColor="danger">
-            <ListItem>Now and Then</ListItem>
-            <ListItem>Pirates of the Caribbean: The Curse of the Black Pearl</ListItem>
-            <ListItem>Bend It Like Beckham</ListItem>
-            <ListItem>Happy-Go-Lucky</ListItem>
-            <ListItem>13 Going on 30</ListItem>
-            
-          </UnorderedList> */}
+          {nominateList.length > 3 ? (
+  <div>
+  {nominateList.map((movie) => (
+    <UnorderedList icon={BanCircleIcon} iconColor="danger">
+    <ListItem>{movie.Title}</ListItem>
+    
+  </UnorderedList>
+  ))}
+ </div> ) : (
+    <div className="errorMessage">
+    <Alert intent="danger" title="You don't have any nominations yet!" />
+    </div>
+ )}
+        
+          
 
          
           <div>
